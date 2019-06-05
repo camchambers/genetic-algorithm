be3bac25d38c8ce1304c24641b3d41be8194a4d2
Cameron Chambers <cameronchambers@trentu.ca>
2019-06-05 08:44:53 -0400

    Completed organism fitness calculation method

--- Organism.cs
+++ Organism.cs
@@ -8,18 +8,36 @@ namespace GenticAlgorithm
 
         int fitness = 0;
         int[] genes = new int[10];
-        int geneLength = 10;
 
+        // Constructor for organism class
         public Organism()
         {
             Random rnd = new Random();
 
-            // Initialize genes randomly
+            // Initialize genes randomly by iterating over all of the genes
+            // and creating a binary string of size geneLength
             for (int i = 0; i < genes.Length; i++)
             {
                 genes[i] = Math.Abs(rnd.Next() % 2);
             }
+        }
 
+        // The fitness method determines the fitness of an organism (ability to
+        // compete with other individuals) based on its genes.
+        // It applies a fitness score to the organism and the probability that
+        // the organism will be selected for reproduction is based on its
+        // fitness score.
+        // In this case, the fitness score will favour individuals that contain
+        // 1's in the binary representation of their genes.
+        public void CalculateFitness()
+        {
+            for (int i = 0; i < genes.Length; i++)
+            {
+                if (genes[i] == 1)
+                {
+                    fitness++;
+                }
+            }
         }
 
 
