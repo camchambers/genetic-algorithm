1b355ab3706df37e55e13b397431b1be59434398
Cameron Chambers <cameronchambers@trentu.ca>
2019-06-21 16:11:07 -0400

    Created a gene class

---
+++ Gene.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace GenticAlgorithm
+{
+    // A gene contains the genetic information that describes an organism
+    // A gene is it's own class so that it is interchangable and can contain
+    // various representations
+    class Genes
+    {
+        private int[] genes {get; set;} = new int[10];
+
+        // Used during reproduction passing a portion of the genetic
+        // information from the genes
+        public int[] getChromosones(){
+         return null;
+        }
+
+    }
+}
